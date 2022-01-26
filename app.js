5dd7e8545f75ac8f918d5822835437130fba8289
azcro <email@example.com>
2022-01-26 14:02:26 -0500

    patra

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var moment = require('moment');
 
-const host = '127.0.0.1';
+const host = '0.0.0.0';
 const port = 3000;
  
 const server = http.createServer((req, res) => {
