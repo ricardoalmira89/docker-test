ce63ccf80bc9b8d882ed2a49c1e55a74ec6c2f4d
azcro <email@example.com>
2022-01-26 13:11:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+var moment = require('moment');
+
+const host = '0.0.0.0';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!0000000');
+});
+
+server.listen(port, host, () => {
+   console.log('Web server running at http://%s:%s',host,port );
+});
